5a61dc24630bd177b5afcb9f9646fb6eb2077f69
Leo <bredleff@yandex.com>
2022-08-12 20:55:55 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import reducers from "./reducers";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./components/App"
+
+const store = configureStore({
+    reducer: reducers,
+    middleware: getDefaultMiddleware => getDefaultMiddleware({
+        serializableCheck: false
+    }),
+    devTools: window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+})
+
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <Provider store={store}>
+        <App />
+    </Provider>
+    
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
